8619d69261c081436e5c58ba770e602cb845b522
D-R4 <ibrahimtsuev1@mail.ru>
2023-03-22 15:57:34 +0300

    storybook

--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -7,12 +7,12 @@ function Switch() {
     const swtchChange = () => {
         setSwtch(!swtch)
     }
-
+    console.log('btn-deg' && swtch)
     return(
         <button className='btn-switch' onClick={swtchChange}>
-            <span className={swtch ? 'btn-deg' : ''}>F</span>
+            <span className={swtch && 'btn-deg'}>F</span>
             <div className={swtch ? 'btn-inside' : 'btn-inside right'}></div>
-            <span className={swtch ? '' : 'btn-deg'}>C</span>
+            <span className={!swtch && 'btn-deg'}>C</span>
         </button>
     )
     
